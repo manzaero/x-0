21f7d3d781f1e894796aed26f6c1d80f2cbe0bdf
manzaero <ligoero@gmail.com>
2024-10-21 22:11:19 +0300

    recover commit

--- src/GameLayout.jsx
+++ src/GameLayout.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 export function GameLayout({restart, togglePlayer, isDraw, isGameEnded, field}) {
     return (
         <div className="container">
-            <button className="btn-restart" onClick={restart}>Начать заново
+            <button className="btn-restart" onClick={restart}>Заново
             </button>
             <button className="btn-restart btn-color" disabled={isDraw || isGameEnded || field.some((param) => param !== '') ?  true : null} onClick={togglePlayer}>Сменить игрока
             </button>
